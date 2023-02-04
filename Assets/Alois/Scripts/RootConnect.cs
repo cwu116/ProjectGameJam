79c2d8f650f49320db594ced7a221705ab1559f4
AAAAAlois <16300240028@fudan.edu.cn>
2023-02-04 19:30:32 +0000

    rotate root

--- Assets/Alois/Scripts/RootConnect.cs
+++ Assets/Alois/Scripts/RootConnect.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RootConnect : MonoBehaviour
 {
+    [SerializeField] GameObject finishButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +22,12 @@ public class RootConnect : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("can connect");
+        finishButton.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("can not connect");
+        finishButton.SetActive(false);
     }
 }
