87e84a4e63ac3b616e0d18a8d2b8df64921f0683
wuchengjun <973736519@qq.com>
2023-02-04 19:29:27 +0000

    添加随机树枝功能

--- Assets/Main/Script/BranchManager.cs
+++ Assets/Main/Script/BranchManager.cs
@@ -7,13 +7,13 @@ public class BranchManager : MonoBehaviour
 {
     public List<GameObject> branchList;
     public List<Transform> branchUIPos;
-    public List<GameObject> branchUIList;
+    public List<Branch> branchUIList;
     // Start is called before the first frame update
     void Start()
     {
+        CreateNewBranch(0);
         CreateNewBranch(1);
         CreateNewBranch(2);
-        CreateNewBranch(3);
     }
 
     // Update is called once per frame
@@ -24,8 +24,9 @@ public class BranchManager : MonoBehaviour
 
     public void CreateNewBranch(int branchindex)
     {
-        branchindex -= 1;
-        GameObject uiBranch = Instantiate<GameObject>(branchUIList[branchindex]);
+        int rand = Random.Range(0, 4);
+        Branch uiBranch = Instantiate<Branch>(branchUIList[rand]);
+        uiBranch .branchindex = branchindex;
         uiBranch.transform.SetParent(branchUIPos[branchindex]);
         uiBranch.transform.position = branchUIPos[branchindex].position;
         uiBranch.transform.localScale = branchUIPos[branchindex].localScale;
